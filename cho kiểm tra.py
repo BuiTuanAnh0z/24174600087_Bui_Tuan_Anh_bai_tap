53126051073bb54743cf2878cc7ee1602c772778
BuiTuanANh0z <btanh.24174600087@sv.uneti.edu.vn>
2024-10-21 19:10:56 +0700

    hoàn thành bài tập chương 6

---
+++ cho kiểm tra.py
@@ -0,0 +1,8 @@
+#Bước 1 : NHập h và r 
+#Bước 2 : Xét điều kiện h và r
+#Nếu h>0 hoặc r>0 đúng=>chuyển sang bước 3 
+#Nếu sai =>
+#Bước 3 : pi = 3,14 
+#Bước 4 : Tính diện tích toàn phần,diện tích xung quanh 
+#Bước 5 :In kết qủa Bước 4 
+#Bước 6 : Kết thúc 
